098c11fbf3cc36e8e4b778864da21c3c705604fc
T00199564 <jack.odriscoll@liebherr.com>
2024-05-14 12:50:10 +0000

    lab 1 mbed OS error
    Signed-off-by: T00199564 <jack.odriscoll@liebherr.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "mbed.h"
 
+InterruptIn buttn(PC_13);
+
+
+void buttn_fall(){
+    printf("button pressed!!\r\n");
+}
+
 // main() runs in its own thread in the OS
 int main()
 {
+    printf("This is mbed os v%d \r\n", MBED_VERSION);
+
+
+    buttn.fall(&buttn_fall);
     while (true) {
 
     }
