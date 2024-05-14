ec9a90fa8310ffd4a00658ad72910f9761c4262f
T00199564 <jack.odriscoll@liebherr.com>
2024-05-14 13:38:49 +0000

    lab 1 code works  i just need to add my comments!
    Signed-off-by: T00199564 <jack.odriscoll@liebherr.com>

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include "mbed.h"
+// Include the Mbed library
 
-InterruptIn buttn(PC_13);
+InterruptIn buttn(PC_13);   // Define an InterruptIn object named buttn, connected to pin PC_13
 
+volatile int myflag = 0;
 
 void buttn_fall(){
-    printf("button pressed!!\r\n");
+    myflag = 1;
 }
 
 // main() runs in its own thread in the OS
 int main()
 {
-    printf("This is mbed os v%d \r\n", MBED_VERSION);
-
-
+    printf("This is mbed os v%d \r\n", MBED_VERSION); // Print the Mbed OS version
     buttn.fall(&buttn_fall);
     while (true) {
+        // Infinite loop
+        
+        if(myflag == 1){
+            printf("button pressed!!\r\n"); // Print a message indicating that the button has been pressed
+            myflag = 0; // Reset the myflag variable to 0
+        }
 
     }
 }
